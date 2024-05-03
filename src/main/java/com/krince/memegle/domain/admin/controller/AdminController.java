e3007199925f0bf3abe0d257b7a74f35d36e774d
kimdonghyun <ssk0080@gmail.com>
2024-05-03 22:38:15 +0900

    #3 밈 이미지 승인 및 반려 처리 명세서 작성

--- src/main/java/com/krince/memegle/domain/admin/controller/AdminController.java
+++ src/main/java/com/krince/memegle/domain/admin/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.krince.memegle.domain.admin.controller;
 
 import com.krince.memegle.domain.admin.dto.request.RequestAdminLoginDto;
+import com.krince.memegle.domain.admin.dto.response.RequestConfirmMimeImageDto;
 import com.krince.memegle.domain.admin.dto.response.ResponseGetAdminPostsDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,15 +22,22 @@ import java.util.List;
 @Tag(name = "관리자", description = "관리자 관련 API")
 public interface AdminController {
 
-    @PostMapping("/sign/in")
     @Operation(summary = "관리자 로그인", description = "관리자 계정에 로그인합니다.")
     @ApiResponse(description = "로그인 성공", responseCode = "204", content = @Content(schema = @Schema(implementation = Void.class)))
     ResponseEntity<Void> login(@RequestBody RequestAdminLoginDto requestAdminLoginDto);
 
-    @GetMapping("/posts")
+    @Operation(summary = "밈 이미지 승인 및 등록 & 반려 처리", description = "해당 밈 이미지를 회원 게시판에 등록 및 반려처리합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "등록 및 반려 처리 성공", content = @Content(schema = @Schema(implementation = Void.class)))
+    })
+    ResponseEntity<Void> confirmMimeImage(
+            @Parameter(description = "밈 이미지 고유번호", example = "1") @PathVariable Long mimeImageId,
+            @RequestBody RequestConfirmMimeImageDto requestConfirmMimeImageDto
+    );
+
     @Operation(summary = "관리자 페이지 홈 화면 조회", description = "관리자 페이지 홈 화면(게시글 목록) 목록을 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseGetAdminPostsDto.class))))
+            @ApiResponse(responseCode = "200", description = "홈 화면 조회 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ResponseGetAdminPostsDto.class))))
     })
     ResponseEntity<List<ResponseGetAdminPostsDto>> getAdminPosts();
 }
--- src/main/java/com/krince/memegle/domain/admin/controller/AdminControllerImpl.java
+++ src/main/java/com/krince/memegle/domain/admin/controller/AdminControllerImpl.java
@@ -1,6 +1,7 @@
 package com.krince.memegle.domain.admin.controller;
 
 import com.krince.memegle.domain.admin.dto.request.RequestAdminLoginDto;
+import com.krince.memegle.domain.admin.dto.response.RequestConfirmMimeImageDto;
 import com.krince.memegle.domain.admin.dto.response.ResponseGetAdminPostsDto;
 import com.krince.memegle.domain.admin.service.AdminService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,6 +22,12 @@ public class AdminControllerImpl implements AdminController {
     @Override
     @PostMapping("/sign/in")
     public ResponseEntity<Void> login(RequestAdminLoginDto requestAdminLoginDto) {
+        return null;
+    }
+
+    @Override
+    @PostMapping("/images/{mimeImageId}")
+    public ResponseEntity<Void> confirmMimeImage(Long mimeImageId, RequestConfirmMimeImageDto requestConfirmMimeImageDto) {
         return null;
     }
 
