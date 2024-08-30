6b3b0497fac89a193f10d90517930b9ccbd59d6b
kimdonghyun <ssk0080@gmail.com>
2024-08-31 07:55:40 +0900

    #42 feat: 회원 서비스 개발

--- src/main/java/com/krince/memegle/domain/user/service/UserService.java
+++ src/main/java/com/krince/memegle/domain/user/service/UserService.java
@@ -1,4 +1,9 @@
 package com.krince.memegle.domain.user.service;
 
+import com.krince.memegle.domain.user.dto.request.SignUpDto;
+import com.krince.memegle.domain.user.entity.User;
+
 public interface UserService {
+
+    public void signUp(SignUpDto signUpDto);
 }
--- src/main/java/com/krince/memegle/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/krince/memegle/domain/user/service/UserServiceImpl.java
@@ -1,7 +1,12 @@
 package com.krince.memegle.domain.user.service;
 
+import com.krince.memegle.domain.user.dto.request.SignUpDto;
+import com.krince.memegle.domain.user.entity.User;
 import com.krince.memegle.domain.user.repository.UserRepository;
+import com.krince.memegle.global.Role;
+import com.krince.memegle.global.exception.DuplicateUserException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +14,35 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Override
+    public void signUp(SignUpDto signUpDto) {
+        validateDuplicateUser(signUpDto.getLoginId());
+
+        Role role = Role.ROLE_USER;
+        User user = signUpDtoToUser(signUpDto, role);
+
+        userRepository.save(user);
+    }
+
+    private void validateDuplicateUser(String loginId) {
+        boolean isExistUser = userRepository.existsByLoginId(loginId);
+
+        if (isExistUser) {
+            throw new DuplicateUserException();
+        }
+    }
+
+    private User signUpDtoToUser(SignUpDto signUpDto, Role role) {
+        String encodedPassword = passwordEncoder.encode(signUpDto.getPassword());
+
+        return User.builder()
+                .loginId(signUpDto.getLoginId())
+                .password(encodedPassword)
+                .name(signUpDto.getName())
+                .nickname(signUpDto.getNickname())
+                .role(role)
+                .build();
+    }
 }
