3292d6b94b2b0cab3c4493e6580ab22d8ec64c9f
kimdonghyun <ssk0080@gmail.com>
2024-08-31 19:02:01 +0900

    #42 feat: 회원 리포지토리 로그인 아이디로 조회 개발

--- src/main/java/com/krince/memegle/domain/user/repository/UserRepository.java
+++ src/main/java/com/krince/memegle/domain/user/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.krince.memegle.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     boolean existsByLoginId(String loginId);
+
+    Optional<User> findByLoginId(String loginId);
 }
