f9835d62f5f66f12d72d735dad30d19b061fc2ef
kimdonghyun <ssk0080@gmail.com>
2024-08-31 19:07:07 +0900

    #42 feat: 회원 컨트롤러 개발
    로그인 기능 개발

--- src/main/java/com/krince/memegle/domain/user/controller/UserController.java
+++ src/main/java/com/krince/memegle/domain/user/controller/UserController.java
@@ -1,18 +1,21 @@
 package com.krince.memegle.domain.user.controller;
 
+import com.krince.memegle.domain.user.dto.request.SignInDto;
 import com.krince.memegle.domain.user.dto.request.SignUpDto;
 import com.krince.memegle.global.response.ResponseCode;
-import com.krince.memegle.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 @Tag(name = "회원", description = "회원 관련 API")
 public interface UserController {
 
     @Operation(summary = "회원 등록", description = "회원가입을 진행합니다.")
-    @ApiResponse(description = "회원 등록 성공", responseCode = "200")
+    @ApiResponse(description = "회원 등록 성공", responseCode = "204")
     ResponseEntity<ResponseCode> signUp(SignUpDto signUpDto);
+
+    @Operation(summary = "회원 로그인", description = "로그인을 진행합니다.")
+    @ApiResponse(description = "회원 로그인 성공", responseCode = "204")
+    ResponseEntity<ResponseCode> signIn(SignInDto signInDto);
 }
