7f056a69fce4c3ffd0fa920da813fd5deea9bf12
kimdonghyun <ssk0080@gmail.com>
2024-09-23 16:17:55 +0900

    #45 feat: ImageController 이미지 업로드 요청 메서드 개발
    ImageController: 이미지 업로드 요청 메서드 개발
    해당 컨트롤러 테스트 코드 작성

--- src/main/java/com/krince/memegle/domain/image/controller/ImageController.java
+++ src/main/java/com/krince/memegle/domain/image/controller/ImageController.java
@@ -3,15 +3,24 @@ package com.krince.memegle.domain.image.controller;
 import com.krince.memegle.domain.image.dto.ViewImageDto;
 import com.krince.memegle.global.ImageCategory;
 import com.krince.memegle.global.dto.PageableDto;
+import com.krince.memegle.global.response.ResponseCode;
 import com.krince.memegle.global.response.SuccessResponse;
 import com.krince.memegle.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Tag(name = "이미지", description = "이미지 관련 API")
@@ -27,7 +36,17 @@ public interface ImageController {
     @Operation(summary = "카테고리 이미지 리스트 조회", description = "선택한 카테고리의 이미지 리스트를 조회합니다.")
     @ApiResponse(description = "카테고리 이미지 리스트 조회 성공", responseCode = "200")
     ResponseEntity<SuccessResponse<List<ViewImageDto>>> getCategoryImages(
-            ImageCategory imageCategory,
-            PageableDto pageableDto,
+            @RequestParam ImageCategory imageCategory,
+            @ModelAttribute @Valid PageableDto pageableDto,
             @Parameter(hidden = true) CustomUserDetails userDetails);
+
+    @Operation(summary = "밈 이미지 등록 요청", description = "밈 이미지를 등록 요청합니다.")
+    @ApiResponse(description = "밈 이미지 등록 요청 성공", responseCode = "204")
+    ResponseEntity<ResponseCode> registMemeImage(
+            @RequestParam ImageCategory imageCategory,
+            @RequestPart MultipartFile memeImage,
+            @RequestPart @NotBlank String tags,
+            @RequestPart @NotNull String delimiterFile,
+            @Parameter(hidden = true) CustomUserDetails userDetails
+            ) throws IOException;
 }
