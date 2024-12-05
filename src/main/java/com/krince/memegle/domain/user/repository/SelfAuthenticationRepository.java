815afdcee27cc2e9a8fbbf6509e85743c74f74e3
kimdonghyun <98032384+dev-krince@users.noreply.github.com>
2024-12-05 13:10:27 +0900

    feat: #66 회원 삭제 repository 개발

--- src/main/java/com/krince/memegle/domain/user/repository/SelfAuthenticationRepository.java
+++ src/main/java/com/krince/memegle/domain/user/repository/SelfAuthenticationRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SelfAuthenticationRepository extends JpaRepository<SelfAuthentication, Long> {}
+public interface SelfAuthenticationRepository extends JpaRepository<SelfAuthentication, Long> {
+
+    void deleteByUserId(Long userId);
+}
